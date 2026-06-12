31324d6208f14474389e9f16e9356c970bfe9f1a
Umar Musa Isah <engrumarmusa@gmail.com>
2026-06-12 15:12:43 +0100

    Removed old files, preparing for new upload

--- core/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-ArewaSchool Manager - Core Module
-"""
-from .database import Database
-from .config import Config
-from .utils import Utils
-
-__all__ = ['Database', 'Config', 'Utils']
--- ui/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-UI Module - User Interface components
-"""
-from .splash import SplashScreen
-from .login import LoginUI
-from .dashboard import DashboardUI
-
-__all__ = ['SplashScreen', 'LoginUI', 'DashboardUI']
